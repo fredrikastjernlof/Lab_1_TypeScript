5dd6c9f576af6364a23a8a8c8b21c6839cd753ce
fredrikastjernlof <fredrika.stjernlof@live.se>
2026-04-05 18:10:07 +0200

    Populate course select with data from JSON

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,29 @@ import data from "./data/courses.json";
 console.log(data);
 
 // En kurs har en kod, ett namn, en progression och en kursplan. Progressionen kan vara A, B eller C.
-type Progression = "A" | "B" | "C" ;
+type Progression = "A" | "B" | "C";
 
 // Skapar en mall för hur en kurs ska se ut. 
-interface CourseInfo{
+interface CourseInfo {
   code: string;
   name: string;
   progression: Progression;
   syllabus: string;
 }
+
+interface RawCourse {
+  code: string;
+  coursename: string;
+  progression: Progression;
+  syllabus: string;
+}
+
+const availableCourses: CourseInfo[] = (data as RawCourse[]).map((course) => ({
+  code: course.code.toUpperCase(),
+  name: course.coursename,
+  progression: course.progression,
+  syllabus: course.syllabus
+}));
 
 // En kurs får bara innehålla CourseInfo-objekt. 
 const courses: CourseInfo[] = [];
@@ -20,10 +34,12 @@ const courses: CourseInfo[] = [];
 console.log(courses);
 
 const form = document.getElementById("courseForm") as HTMLFormElement;
+const courseSelect = document.getElementById("courseSelect") as HTMLSelectElement;
 const codeInput = document.getElementById("code") as HTMLInputElement;
 const nameInput = document.getElementById("name") as HTMLInputElement;
 const progressionSelect = document.getElementById("progression") as HTMLSelectElement;
 const syllabusInput = document.getElementById("syllabus") as HTMLInputElement;
+const courseList = document.getElementById("courseList") as HTMLUListElement;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -35,5 +51,35 @@ form.addEventListener("submit", (e) => {
     syllabus: syllabusInput.value
   };
 
+  courses.push(newCourse);
+  renderCourses();
   console.log(newCourse);
-})
+});
+
+function populateCourseSelect(): void {
+  availableCourses.forEach((course) =>{
+    const option =document.createElement("option");
+    option.value = course.code;
+    option.textContent = `${course.code} - ${course.name}`;
+    courseSelect.appendChild(option);
+  });
+}
+
+function renderCourses(): void {
+
+  //Rensa tidigare lista
+  courseList.innerHTML = "";
+
+  courses.forEach((course) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+    <strong>${course.code}</strong> - ${course.name} (${course.progression})
+    <br>
+    <a href="${course.syllabus}" target="_blank"> Kursplan</a>
+    `;
+
+    courseList.appendChild(li);
+  });
+}
+
+populateCourseSelect();
